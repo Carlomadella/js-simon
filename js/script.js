0a0043833bfe70a75599f536ce521e9eacf83876
Carlomadella <madella871@gmail.com>
2025-03-27 14:37:29 +0100

    creati file necessari compreso readme

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// SCALETTA:
+
+// riprendere dal dom la lista con la formula const numbers = document.getElementById("numbers-list");
+
+// Creo una funzione per:
+//     generare 5 numeri casuali da 1 a 50
+//     memorizzare la lista di numeri in un array
+//     stampare i numeri dell'array come elementi HTML (li) nella lista (ul)
+
+// Genero il timer con setTimeout:
+//     il timer deve durare 30 secondi
+
+// Creo una funzione che:
+//     passato il tempo prestabilito fa sparire i numeri dalla lista dandole display-none
+//     faccia comparire i 5 input a cui bisogna cambiare display-none con display-block
+    
+
+// NOTA: l'ordine in cui i numeri sono inseriti dall'utente non importa
+
+// il programma deve rispondere quanti e quali numeri sono stati indovinati:
+
+// Creo un addEventListener al click del bottone:
+//     riceva l'input dei numeri scelti riprendendo il suo valore (.value()) su javascript
+//     conservi il valore in un array
+//     Creo un ciclo for:
+//     confronto ogni singolo elemento dell'array creato all'inizio con ogni singolo elemento dell'array appena creato
+//     stampo il risultato nel p con id="message"
+
+// INIZIO ESERCIZIO
+
